e01bbd139bc050a1418333ada50b13c8913d0d26
Micael Santos <trabalhodigitaltek@gmail.com>
2024-09-26 14:09:46 -0300

    ajustes 0.4

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,22 +1,33 @@
-const elements = document.querySelectorAll(".fadeL");
+// Efeitos de Fade
+function handleAnimations() {
+  const elements = document.querySelectorAll('.fadeL, .fadeR');
 
-elements.forEach((element) => {
-  ScrollReveal().reveal(element, {
-    origin: "left",
-    duration: 2000,
-    distance: "20%",
+  elements.forEach(element => {
+    const revealConfig = {
+      duration: 2000,
+    };
+
+    if (window.innerWidth <= 768) {
+      // Para telas menores ou iguais a 768px
+      revealConfig.origin = 'bottom';
+      revealConfig.distance = '20%';
+    } else {
+      // Para telas maiores que 768px
+      if (element.classList.contains('fadeL')) {
+        revealConfig.origin = 'left';
+      } else {
+        revealConfig.origin = 'right';
+      }
+      revealConfig.distance = '20%';
+    }
+
+    ScrollReveal().reveal(element, revealConfig);
   });
-});
+}
 
-const elements2 = document.querySelectorAll(".fadeR");
-
-elements2.forEach((element) => {
-  ScrollReveal().reveal(element, {
-    origin: "right",
-    duration: 2000,
-    distance: "20%",
-  });
-});
+// Chamar a função quando a página carregar e sempre que o tamanho da janela mudar
+window.onload = handleAnimations;
+window.addEventListener("resize", handleAnimations);
 
 // Garante que a página role para o topo após todos os recursos serem carregados
 $(window).on("load", function () {
